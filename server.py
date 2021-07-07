40bd0d3e8be12a837f30f59d3c648db8b5d7b2bc
Leonardo Luís Dalcegio <leoodalcegio@hotmail.com>
2021-07-07 20:07:56 +0000

    wip

--- server.py
+++ server.py
@@ -37,6 +37,9 @@ def predict_func():
     return jsonify(output)
 
 
+if __name__ == "__main__":
+  app.run(port=3000, debug=True)
+
 """
 
 sentences = [["Formula One (also known as Formula 1 or F1) is the highest class of international auto racing for single-seater formula racing cars sanctioned by the Fédération Internationale de l'Automobile (FIA). The World Drivers' Championship, which became the FIA Formula One World Championship in 1981, has been one of the premier forms of racing around the world since its inaugural season in 1950. The word formula in the name refers to the set of rules to which all participants' cars must conform. A Formula One season consists of a series of races, known as Grands Prix, which take place worldwide on both purpose-built circuits and closed public roads."
