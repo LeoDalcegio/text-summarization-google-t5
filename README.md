fc54d45a12cc2e28a4505d1adeb951ef5fdb72f2
Leonardo Luís Dalcegio <leoodalcegio@hotmail.com>
2021-07-07 19:47:27 +0000

    wip

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # text-summarization-google-t5
 Text Summarization with Google AI's T5 in Python
 
-
+You need to have Java 8 (Oracle or OpenJDK)
---
+++ server.py
@@ -0,0 +1,50 @@
+from flask import Flask, jsonify, request
+from sparknlp.base import DocumentAssembler
+from sparknlp.annotator import T5Transformer
+from pyspark.ml import Pipeline
+from pyspark.sql import SparkSession
+import sparknlp
+
+spark = sparknlp.start()
+
+document_assembler = DocumentAssembler() \
+    .setInputCol("text") \
+    .setOutputCol("documents")
+
+t5 = T5Transformer() \
+    .pretrained("t5_base") \
+    .setTask("summarize:")\
+    .setMaxOutputLength(100)\
+    .setInputCols(["documents"]) \
+    .setOutputCol("summaries")
+
+pipe_components = [document_assembler, t5]
+pipeline = Pipeline().setStages(pipe_components)
+
+app = Flask(__name__)
+
+
+@app.route('/predict', methods=['POST'])
+def predict_func():
+    test_data = request.get_json()
+
+    data_df = spark.createDataFrame(test_data.sentences).toDF("text")
+
+    results = pipeline.fit(data_df).transform(data_df)
+
+    output = results.select("summaries.result").collect(truncate=False)
+
+    return jsonify(output)
+
+
+"""
+
+sentences = [["Formula One (also known as Formula 1 or F1) is the highest class of international auto racing for single-seater formula racing cars sanctioned by the Fédération Internationale de l'Automobile (FIA). The World Drivers' Championship, which became the FIA Formula One World Championship in 1981, has been one of the premier forms of racing around the world since its inaugural season in 1950. The word formula in the name refers to the set of rules to which all participants' cars must conform. A Formula One season consists of a series of races, known as Grands Prix, which take place worldwide on both purpose-built circuits and closed public roads."
+              " "
+              "The results of each race are evaluated using a points system to determine two annual World Championships: one for drivers, the other for constructors. Each driver must hold a valid Super Licence, the highest class of racing licence issued by the FIA. The races must run on tracks graded 1 (formerly A), the highest grade-rating issued by the FIA. Most events occur in rural locations on purpose-built tracks, but several events take place on city streets."
+              " "
+              "Formula One cars are the fastest regulated road-course racing cars in the world, owing to very high cornering speeds achieved through the generation of large amounts of aerodynamic downforce. The cars underwent major changes in 2017, allowing wider front and rear wings, and wider tyres, resulting in peak cornering forces near 6.5 lateral g and top speeds of around 350 km/h (215 mph). As of 2021, the hybrid engines are limited in performance to a maximum of 15,000 rpm; the cars are very dependent on electronics and aerodynamics, suspension and tyres. Traction control, launch control, and automatic shifting, plus other electronic driving aids, were first banned in 1994, reintroduced in 2001, and have more recently been banned since 2004 and 2008, respectively."
+              " "
+              "While Europe is the sport's traditional base, the championship operates globally, with 13 of the 23 races in the 2021 season taking place outside Europe. With the annual cost of running a mid-tier team – designing, building, and maintaining cars, pay, transport – being US$120 million, its financial and political battles are widely reported. Its high profile and popularity have created a major merchandising environment, which has resulted in large investments from sponsors and budgets (in the hundreds of millions for the constructors). On 23 January 2017, Liberty Media confirmed the completion of the acquisition of Delta Topco, the company that controls Formula One, from private-equity firm CVC Capital Partners for $8 billion."]]
+
+"""
